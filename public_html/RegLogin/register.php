90740eb4724d5134f9180dddc4a31357a2698bb9
pd0225 <pd389@njit.edu>
2020-06-03 15:59:12 -0400

    WIP of register

---
+++ public_html/RegLogin/register.php
@@ -0,0 +1,18 @@
+<form method="POST">
+	<label for="email">Email
+	<input type="email" id="email" name="email"/>
+	</label>
+	<label for="p">Password
+	<input type="password" id="p" name="password"/>
+	</label>
+	<label for="cp">Confirm Password
+	<input type="password" id="cp" name="cpassword/>
+	</label>
+	<input type="submit" name="register" value="Register"/>
+</form>
+
+<?php
+echo var_export($_GET, true);
+echo var_export($_POST, true);
+echo var_export($_REQUEST, true);
+?>
