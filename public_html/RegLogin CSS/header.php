efb4dfa4d8b57674d8ea3fdee7b8929e861abbd4
pd0225 <pd389@njit.edu>
2020-06-09 16:30:09 -0400

    RegLogin CSS Start

---
+++ public_html/RegLogin CSS/header.php
@@ -0,0 +1,8 @@
+<head>
+    <title>Preksha's Website</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<?php
+require("config.php");
+session_start();
+?>
--- public_html/RegLogin CSS/index.php
+++ public_html/RegLogin CSS/index.php
@@ -1,4 +1,3 @@
 <?php
 include("header.php");
-?>
-<?php echo "test";?>
+echo "test";?>
