3a7b0202dda204f8ae9df116d8372af86d0ddf8c
pd0225 <pd389@njit.edu>
2020-06-13 15:16:28 -0400

    milestone one

---
+++ public_html/Milestone 1/header.php
@@ -0,0 +1,25 @@
+<head>
+    <title>Preksha's Bank</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <h1>Simple Bank</h1>
+</head>
+<?php
+require("config.php");
+session_start();
+?>
+<nav>
+    <ul>
+        <li>
+            <a href="home.php">Home</a>
+        </li>
+        <li>
+            <a href="login.php">Login</a>
+        </li>
+        <li>
+            <a href="register.php">Register</a>
+        </li>
+        <li>
+            <a href="logout.php">Logout</a>
+        </li>
+    </ul>
+</nav>
