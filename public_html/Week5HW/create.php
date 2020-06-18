9c6e02a9276bf0ecae1f7c329d865bf5dd874ba1
pd0225 <pd389@njit.edu>
2020-06-18 15:46:46 -0400

    Week5HW files

---
+++ public_html/Week5HW/create.php
@@ -0,0 +1,62 @@
+<form method="POST">
+    <label for="name">Account Name
+        <input type="text" id="Name" name="Name" />
+    </label>
+    <label for="AccountNum">Account Number
+        <input type="number" id="AccountNum" name="Account_Number" />
+    </label>
+    <label for="acctype">Account Type
+        <input type="text" id="AccountType" name="Account_Type" />
+    </label>
+    <label for="AccountBalance">Balance
+        <input type="number" id="AccountBalance" name="Account_Balance" />
+    </label>
+    <input type="submit" name="Bank" value="Create Account"/>
+</form>
+<?php
+require("common.inc.php");
+$db = getDB();
+
+
+if(isset($_POST["Bank"])){
+    $name = $_POST["Name"];
+    $AccountNum = $_POST["Account_Number"];
+    $AccountType = $_POST["Account_Type"];
+    $AccountBalance = $_POST["Account_Balance"];
+    if(!empty($name) && !empty($Accnum)&& !empty($Acctyp)&& !empty($balance)){
+        require("config.php");
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+        try{
+            $db = new PDO($connection_string, $dbuser, $dbpass);
+            $stmt = $db->prepare("INSERT INTO Bank_Account (Name, Account_Number, Account_Type,Account_Balance) VALUES (:name, :AccountNum, :AccountType,:AccountBalance)");
+            $result = $stmt->execute(array(
+                ":name" => $name,
+                ":AccountNum" => $AccountNum,
+                ":AccountType"=> $AccountType,
+                ":AccountBalance"=> $AccountBalance
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
+            }
+            else{
+                //echo var_export($result, true);
+                if ($result){
+                    echo "Successfully inserted new thing: " . $name;
+                }
+                else{
+                    echo "Error inserting record";
+                }
+            }
+        }
+        catch (Exception $e){
+            echo $e->getMessage();
+        }
+    }
+    else{
+        echo "<div>Name, Account Number, Account Type and Account Balance must not be empty.<div>";
+    }
+}
+$stmt = $db->prepare("SELECT * FROM Bank_Account");
+$stmt->execute();
+?>
