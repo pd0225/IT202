7c0ebe6cc26933d9eeda2f2aa30118b72aebb734
pd0225 <60520891+pd0225@users.noreply.github.com>
2020-06-21 22:10:52 -0400

    Delete create_table_account.sql

--- public_html/CreateEdit/create_table_account.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Accounts (
-    id int auto_increment,
-    name varchar(20) unique,
-    balance decimal(12,2) default 0.00,
-    created datetime default current_timestamp,
-    modified datetime default current_timestamp on update current_timestamp,
-    primary key (id)
-)
