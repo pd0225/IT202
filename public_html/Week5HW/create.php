41a2a32b9924a0d39051aa61e784a342121273e9
pd0225 <pd389@njit.edu>
2020-06-21 21:01:46 -0400

    adding and editing

--- public_html/Week5HW/create.php
+++ public_html/Week5HW/create.php
@@ -1,47 +1,26 @@
-<?php
-require("config.php");
-$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-$db = new PDO($connection_string, $dbuser, $dbpass);
-$accountId = -1;
-$result = array();
-function get($arr, $key){
-    if(isset($arr[$key])){
-        return $arr[$key];
-    }
-    return "";
-}
-if(isset($_GET["accountId"])){
-    $accountId = $_GET["accountId"];
-    $stmt = $db->prepare("SELECT * FROM Account where id = :id");
-    $stmt->execute([":id"=>$accountId]);
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-}
-else{
-    echo "No accountId provided in url.";
-}
-?>
-
-    <form method="POST">
-        <label for="AccountName">Account Name
-            <input type="text" id="account" name="Account Name" value="<?php echo get($result, "AccountName");?>" />
-        </label>
-        <label for="b">Account Balance
-            <input type="number" id="b" name="Account Balance" value="<?php echo get($result, "AccountBalance");?>" />
-        </label>
-        <input type="submit" name="updated" value="Update Account"/>
-    </form>
+<form method="POST">
+    <label for="account">Account Name
+        <input type="text" id="account" name="AccountName" />
+    </label>
+    <label for="b">Balance
+        <input type="number" id="b" name="AccountBalance" />
+    </label>
+    <input type="submit" name="created" value="Create Account"/>
+</form>
 
 <?php
-if(isset($_POST["updated"])){
+if(isset($_POST["created"])){
     $AccountName = $_POST["AccountName"];
     $AccountBalance = $_POST["AccountBalance"];
-    if(!empty($AccountName) && !empty($deposit)){
+    if(!empty($AccountName) && !empty($AccounBalance)){
+        require("config.php");
+        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
         try{
-            $stmt = $db->prepare("UPDATE Account set name = :name, balance=:balance where id=:id");
+            $db = new PDO($connection_string, $dbuser, $dbpass);
+            $stmt = $db->prepare("INSERT INTO Account (name, AccountBalance) VALUES (:AccountName, :AccountBalance)");
             $result = $stmt->execute(array(
                 ":AccountName" => $AccountName,
-                ":AccountBalance" => $AccountBalance,
-                ":id" => $accountId
+                ":AccountBalance" => $AccountBalance
             ));
             $e = $stmt->errorInfo();
             if($e[0] != "00000"){
@@ -50,10 +29,10 @@ if(isset($_POST["updated"])){
             else{
                 echo var_export($result, true);
                 if ($result){
-                    echo "Successfully updated account: " . $AccountName;
+                    echo "Successfully created new account: " . $AccountName;
                 }
                 else{
-                    echo "Error updating account";
+                    echo "Error creating account";
                 }
             }
         }
