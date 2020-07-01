a2c138b28424b6d17454ac1ade19656edd3b67d5
pd0225 <pd389@njit.edu>
2020-07-01 18:17:44 -0400

    making changes

--- public_html/Milestone2/edit.php
+++ public_html/Milestone2/edit.php
@@ -3,97 +3,70 @@ include("header.php");
 ?>
 <h2>Update Bank Account</h2>
 <?php
+require("config.php");
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+$db = new PDO($connection_string, $dbuser, $dbpass);
 $accountId = -1;
-if(isset($_GET["accountId"]) && !empty($_GET["accountId"])){
-    $accountId = $_GET["accountId"];
-}
 $result = array();
-require("common.inc.php");
-?>
-<?php
-if(isset($_POST["updated"])){
-    $name = "";
-    $AccountBalance = -1;
-    if(isset($_POST["name"]) && !empty($_POST["name"])){
-        $name = $_POST["name"];
+function get($arr, $key){
+    if(isset($arr[$key])){
+        return $arr[$key];
     }
-    if(isset($_POST["AccountBalance"]) && !empty($_POST["AccountBalance"])){
-        if(is_numeric($_POST["AccountBalance"])){
-            $quantity = (int)$_POST["AccountBalance"];
-        }
-    }
-    if(!empty($name) && $AccountBalance > -1){
-        try{
-            $query = NULL;
-            echo "[Account Balance" . $AccountBalance . "]";
-            $query = file_get_contents(__DIR__ . "/queries/update_table_accounts.sql");
-            if(isset($query) && !empty($query)) {
-                $stmt = getDB()->prepare($query);
-                $result = $stmt->execute(array(
-                    ":name" => $name,
-                    ":balance" => $AccountBalance,
-                    ":id" => $balanceId
-                ));
-                $e = $stmt->errorInfo();
-                if ($e[0] != "00000") {
-                    echo var_export($e, true);
-                } else {
-                    if ($result) {
-                        echo "Successfully updated account: " . $name;
-                    } else {
-                        echo "Error updating record";
-                    }
-                }
-            }
-            else{
-                echo "Failed to find update_table_accounts.sql file";
-            }
-        }
-        catch (Exception $e){
-            echo $e->getMessage();
-        }
-    }
-    else{
-        echo "Name and account balance must not be empty.";
-    }
+    return "";
+}
+if(isset($_GET["accountId"])){
+    $accountId = $_GET["accountId"];
+    $stmt = $db->prepare("SELECT * FROM Accounts where id = :id");
+    $stmt->execute([":id"=>$accountId]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+else{
+    echo "No accountId provided in url.";
 }
 ?>
 
+<form method="POST">
+    <label for="account">Account Name
+        <input type="text" id="account" name="name" value="<?php echo get($result, "name");?>" />
+    </label>
+    <label for="b">Balance
+        <input type="number" id="b" name="balance" value="<?php echo get($result, "balance");?>" />
+    </label>
+    <input type="submit" name="updated" value="Update Account"/>
+</form>
+
 <?php
-//moved the content down here so it pulls the update from the table without having to refresh the page or redirect
-//now my success message appears above the form so I'd have to further restructure my code to get the desired output/layout
-if($thingId > -1){
-    $query = file_get_contents(__DIR__ . "/queries/select_one_table_accounts.sql");
-    if(isset($query) && !empty($query)) {
-        //Note: SQL File contains a "LIMIT 1" although it's not necessary since ID should be unique (i.e., one record)
-        try {
-            $stmt = getDB()->prepare($query);
-            $stmt->execute([":id" => $accountId]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+if(isset($_POST["updated"])){
+    $name = $_POST["name"];
+    $balance = $_POST["balance"];
+    if(!empty($name) && !empty($balance)){
+        try{
+            $stmt = $db->prepare("UPDATE Accounts set name = :name, balance=:balance where id=:id");
+            $result = $stmt->execute(array(
+                ":name" => $name,
+                ":balance" => $balance,
+                ":id" => $accountId
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                echo var_export($e, true);
+            }
+            else{
+                echo var_export($result, true);
+                if ($result){
+                    echo "Successfully updated account: " . $name;
+                }
+                else{
+                    echo "Error updating account";
+                }
+            }
         }
         catch (Exception $e){
             echo $e->getMessage();
         }
     }
     else{
-        echo "Failed to find select_one_table_accounts.sql file";
+        echo "Name and balance must not be empty.";
     }
 }
-else{
-    echo "No accountId provided in url, don't forget this or sample won't work.";
-}
 ?>
-<script src="js/script.js"></script>
-<!-- note although <script> tag "can" be self terminating some browsers require the
-full closing tag-->
-<form method="POST"onsubmit="return validate(this);">
-    <label for="name">Account Name
-        <!-- since the last assignment we added a required attribute to the form elements-->
-        <input type="text" id="account" name="name" value="<?php echo get($result, "name");?>" required />
-    </label>
-    <label for="b">Account Balance
-        <!-- We also added a minimum value for our number field-->
-        <input type="number" id="b" name="AccountBalance" value="<?php echo get($result, "AccountBalance");?>" required min="0"/>
-    </label>
-    <input type="submit" name="updated" value="Update Account"/>
-</form>
