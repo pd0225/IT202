4e1784e7f62ef155c9b62a134d4d19ad01cc0f3b
pd0225 <pd389@njit.edu>
2020-07-19 14:46:25 -0400

    updates

--- public_html/Milestone3_Project/transactions.php
+++ public_html/Milestone3_Project/transactions.php
@@ -10,24 +10,24 @@ function do_bank_action($account2, $account3, $amountChange, $acctype){
     require("config.php");
     $conn_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     $db = new PDO($conn_string, $dbuser, $dbpass);
-    $a1total = 0;//TODO get total of account 1
     $a2total = 0;//TODO get total of account 2
+    $a3total = 0;//TODO get total of account 3
     $query = "INSERT INTO `Transactions` (`acc_src_id`, `acc_dest_id`, `amount`, `acctype`, `exp_total`) 
-	VALUES(:p1a1, :p1a2, :p1change, :acctype, :a1total), 
-			(:p2a1, :p2a2, :p2change, :acctype, :a2total)";
+	VALUES(:p1a2, :p1a3, :p1change, :acctype, :a2total), 
+			(:p2a2, :p2a3, :p2change, :acctype, :a3total)";
 
     $stmt = $db->prepare($query);
-    $stmt->bindValue(":p1a1", $account2);
-    $stmt->bindValue(":p1a2", $account3);
+    $stmt->bindValue(":p1a2", $account2);
+    $stmt->bindValue(":p1a3", $account3);
     $stmt->bindValue(":p1change", $amountChange);
     $stmt->bindValue(":acctype", $acctype);
-    $stmt->bindValue(":a1total", $a1total);
+    $stmt->bindValue(":a2total", $a1total);
     //flip data for other half of transaction
-    $stmt->bindValue(":p2a1", $account3);
-    $stmt->bindValue(":p2a2", $account2);
+    $stmt->bindValue(":p2a2", $account3);
+    $stmt->bindValue(":p2a3", $account2);
     $stmt->bindValue(":p2change", ($amountChange*-1));
     $stmt->bindValue(":acctype", $acctype);
-    $stmt->bindValue(":a2total", $a2total);
+    $stmt->bindValue(":a3total", $a2total);
     $result = $stmt->execute();
     echo var_export($result, true);
     echo var_dump($account2, true);
@@ -39,7 +39,7 @@ function do_bank_action($account2, $account3, $amountChange, $acctype){
     <form method="POST">
         <input type="text" name="account2" placeholder="Account ID">
         <!-- If our sample is a transfer show other account field-->
-        <?php if($_GET['type'] == 'transfer') : ?>
+        <?php if($_GET['acctype'] == 'transfer') : ?>
             <input type="text" name="account3" placeholder="Other Account ID">
         <?php endif; ?>
 
@@ -56,7 +56,7 @@ function do_bank_action($account2, $account3, $amountChange, $acctype){
 <?php
 if(empty($transfer))
     $transfer= '000000000000';
-else $type = "Transfer";
+else $acctype = "Transfer";
 if(isset($_POST['acctype']) && isset($_POST['account1']) && isset($_POST['amount'])){
     $acctype = $_POST['acctype'];
     $amount = (int)$_POST['amount'];
