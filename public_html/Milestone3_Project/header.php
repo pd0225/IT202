5af84deaa6d9a192d3a1d2fce9de2a7c527fc1a2
pd0225 <pd389@njit.edu>
2020-07-23 11:02:34 -0400

    updating project

--- public_html/Milestone3_Project/header.php
+++ public_html/Milestone3_Project/header.php
@@ -1,7 +1,7 @@
 <head>
     <title>Simple Bank</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <img id="psb" src="psblogo.png" alt="Simple Bank Logo" height="140" width="160" align="left" style="margin-left: 39pc;">
+    <img id="psb" src="psb.jpg" alt="Simple Bank Logo" height="140" width="160" align="left" style="margin-left: 39pc;">
     <h1>Preksha's Simple Bank</h1>
 </head>
 <?php
