1a655f0be6f071289e74ab91dfbed7a611c45548
pd0225 <pd389@njit.edu>
2020-07-25 12:56:14 -0400

    updating

--- public_html/Milestone3_Project/create.php
+++ public_html/Milestone3_Project/create.php
@@ -16,43 +16,134 @@ include("header.php");
         </select>
     </label>
     <label for="balance">Balance
-        <input type="number" id="balance" name="Balance" />
+        <input type="number" id="balance" name="Balance" required min="5"/>
     </label>
     <input type="submit" name="created" value="Create Account"/>
 <?php
-if(isset($_POST["created"])){
-    $name = $_POST["name"];
-    $balance = $_POST["balance"];
-    if(!empty($name) && !empty($balance)){
-        require("config.php");
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+if(isset($_POST["created"])) {
+    $name = "";
+    $acctype = $_POST["acctype"];
+    $balance = -1;
+    $transfer = $_POST["Transfer"];
+    $type = "Deposit";
+    require("config.php");
+    $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+    $db = new PDO($connection_string, $dbuser, $dbpass);
+    $amount=0.0;
+    if(empty($transfer))
+        $transfer= '000000000000';
+    else {
+        $type = "Transfer";
+        $stmt1 = $db->prepare("SELECT * FROM Accounts where acc_num=:acc");
+        $stmt1->execute(array(
+            ":acc" => $transfer
+        ));
+        $result = $stmt1->fetchAll();
+        $amount=$result[0]["Balance"];
+
+
+    }
+    $amount=$amount-$balance;
+    if(!empty($name) && !empty($acctype)&& !empty($balance) && $balance>=5){
+
         try{
-            $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Accounts (name, balance) VALUES (:name, :balance)");
+
+            try{
+                $stmt1 = $db->prepare("SELECT id FROM Users where email = :email LIMIT 1");
+                $stmt1->execute(array(
+                    ":email" => $email
+                ));
+                $res = $stmt1->fetch(PDO::FETCH_ASSOC);
+                $user_id=$res["id"];
+            }catch (Exception $e1){
+                echo $e1->getMessage();
+            }
+            if($acctype == 'Savings')
+                $APY=3.25;
+            else $APY=0.00;
+            $stmt = $db->prepare("INSERT INTO Accounts (Name, acctype, User_id, APY) VALUES (:name, :acctype,:user,:APY)");
             $result = $stmt->execute(array(
                 ":name" => $name,
-                ":balance" => $balance
+                ":acctype"=> $acctype,
+                ":user"=>$user_id,
+                ":APY"=> $APY
             ));
             $e = $stmt->errorInfo();
             if($e[0] != "00000"){
                 echo var_export($e, true);
             }
+            $stmt1 = $db->prepare("SELECT max(id) as id FROM Accounts where Name = :name and acctype=:acctype and User_id=:user");
+            $stmt1->execute(array(
+                ":name" => $name,
+                ":acctype"=> $acctype,
+                ":user"=>$user_id
+            ));
+            $res = $stmt1->fetch(PDO::FETCH_ASSOC);
+            $acc_id=$res["id"];
+            $acc_num=str_pad($acc_id, 12, "0", STR_PAD_LEFT);
+            $stmt = $db->prepare("update Accounts set acc_num=:acc_num where id=:idnum");
+            $result = $stmt->execute(array(
+                ":acc_num" => $acc_num,
+                ":idnum"=>$acc_id
+            ));
+            $balance =$balance * -1;
+            $stmt = $db->prepare("INSERT INTO Transactions (acc_src_id, acc_dest_id,acctype,Amount,exp_total) VALUES (:acc_num,:accnum1, :acctype,:balance,:exp_balance)");
+            $result = $stmt->execute(array(
+                ":acc_num" => $transfer,
+                ":accnum1" => $acc_num ,
+                ":acctype" => $acctype,
+                ":balance" => $balance,
+                ":exp_balance" => $amount
+            ));
+            $e = $stmt->errorInfo();
+            if($e[0] != "00000"){
+                var_dump($e);
+                echo "setting eee ".$e."<br>";
+            }
+            $balance =$balance * -1;
+
+            $stmt2 = $db->prepare("INSERT INTO Transactions (acc_src_id, acc_dest_id,acctype,amount,exp_total) VALUES (:acc1,:acc, :acctype,:balance,:exp_balance)");
+            $result1 = $stmt2->execute(array(
+                ":acc1" => $acc_num,
+                ":acc" => $transfer,
+                ":acctype" => $acctype,
+                ":balance" => $balance,
+                ":exp_balance" => $balance
+            ));
+            $e = $stmt2->errorInfo();
+            if($e[0] != "00000"){
+            }
+            $stmt = $db->prepare("update Accounts set Balance= (SELECT sum(Amount) FROM Transactions WHERE acc_src_id=:acc_num) where acc_num=:acc_num");
+            $result = $stmt->execute(array(
+                ":acc_num" => $acc_num
+            ));
+            if ($result){
+                echo "Successfully created new account: " . $name;
+                $query=$db->prepare("SELECT b.acc_num FROM Accounts b, Users a where a.id=b.User_id and a.email=:email");
+                $query->execute(array(
+                    ":email" => $email
+                ));
+                $res = $query->fetchAll();
+                $_SESSION["user"]["accounts"]=$res;
+                echo var_export($_SESSION, true);
+                header("Location: home.php");
+            }
             else{
-                echo var_export($result, true);
-                if ($result){
-                    echo "Successfully created new account: " . $name;
-                }
-                else{
-                    echo "Error creating account";
-                }
+                echo "Error inserting record";
             }
         }
+
         catch (Exception $e){
+
             echo $e->getMessage();
         }
     }
+
     else{
-        echo "Name and balance must not be empty.";
+
+        echo "<div>Account name, type and balance must not be empty. Balance must be at least 5 dollars.<div>";
     }
 }
+$stmt = $db->prepare("SELECT * FROM Accounts");
+$stmt->execute();
 ?>
---
+++ public_html/Milestone3_Project/transactions.php
@@ -0,0 +1,65 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+function do_bank_action($account1, $account2, $amountChange, $acctype){
+    require("config.php");
+    $conn_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+    $db = new PDO($conn_string, $dbuser, $dbpass);
+    $a1total = 0;//TODO get total of account 1
+    $a2total = 0;//TODO get total of account 2
+    $query = "INSERT INTO `Transactions` (`acc_src_id`, `acc_dest_id`, `amount`, `acctype`, `exp_total`) 
+	VALUES(:p1a1, :p1a2, :p1change, :type, :a1total), 
+			(:p2a1, :p2a2, :p2change, :type, :a2total)";
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue(":p1a1", $account1);
+    $stmt->bindValue(":p1a2", $account2);
+    $stmt->bindValue(":p1change", $amountChange);
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":a1total", $a1total);
+    //flip data for other half of transaction
+    $stmt->bindValue(":p2a1", $account2);
+    $stmt->bindValue(":p2a2", $account1);
+    $stmt->bindValue(":p2change", ($amountChange*-1));
+    $stmt->bindValue(":type", $type);
+    $stmt->bindValue(":a2total", $a2total);
+    $result = $stmt->execute();
+    echo var_export($result, true);
+    echo var_dump($account1, true);
+    echo var_dump ($account2, true);
+    echo var_export($stmt->errorInfo(), true);
+    return $result;
+}
+?>
+    <form method="POST">
+        <input type="text" name="account1" placeholder="Account ID">
+        <!-- If our sample is a transfer show other account field-->
+        <?php if($_GET['acctype'] == 'transfer') : ?>
+            <input type="text" name="account2" placeholder="Other Account ID">
+        <?php endif; ?>
+
+        <input type="number" name="amount" placeholder="$0.00"/>
+        <input type="hidden" name="acctype" value="<?php echo $_GET['acctype'];?>"/>
+
+        <!--Based on sample type change the submit button display-->
+        <input type="submit" value="Transfer Money"/>
+    </form>
+
+<?php
+if(isset($_POST['acctype']) && isset($_POST['account1']) && isset($_POST['amount'])){
+    $type = $_POST['acctype'];
+    $amount = (int)$_POST['amount'];
+    switch($acctype){
+        case 'deposit':
+            do_bank_action("000000000000", $_POST['account1'], ($amount * -1), $acctype);
+            break;
+        case 'withdraw':
+            do_bank_action($_POST['account1'], "000000000000", ($amount * -1), $acctype);
+            break;
+        case 'transfer':
+            //TODO figure it out
+            break;
+    }
+}
+?>
