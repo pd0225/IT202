ff2dcfbdb4a9e8a6d7a32f3d804011c18b2826c3
pd0225 <pd389@njit.edu>
2020-07-26 15:40:40 -0400

    updating

--- public_html/Milestone3_Project/acc_prof.php
+++ public_html/Milestone3_Project/acc_prof.php
@@ -12,13 +12,6 @@ include("header.php");
 </form>
 
 <?php
-if (Common::is_logged_in()) {
-    echo "<h4>User: " . $account . "</h4>";
-    echo "<h4>Account Type: " . $acctype . "</h4>";
-    echo "<h4>Account Balance: " . $amount . "</h4>";
-}
-?>
-<?php
 $email=$_SESSION["user"]["email"];
 $account=$_GET["account"];
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
