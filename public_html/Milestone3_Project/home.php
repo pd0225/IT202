2ed38c07e6e16555c58ffc8767a68d1e69201c13
pd0225 <pd389@njit.edu>
2020-07-26 13:51:21 -0400

    update home

--- public_html/Milestone3_Project/home.php
+++ public_html/Milestone3_Project/home.php
@@ -1,14 +1,5 @@
 <?php
-include("header.php");
-require("common.inc.php");
-require("config.php");
-$accounts=$_SESSION["user"]["accounts"];
-$new_arr = array_column($accounts,'acc_num');
-foreach($new_arr as $item){
-    echo "<a href=acc_prof.php?account=". $item.">".$item."</a>";
-    echo "<a href=Deposit.php?acc_num=". $item.">Deposit</a>";
-    echo "<a href=Withdraw.php?acc_num=".$item.">Withdraw</a>";
-    echo "<a href=Transfer.php?acc_num=".$item.">Transfer</a>";
-    echo '<br>';
-}
+include(__DIR__."/header.php");
 ?>
+    <h4>Home</h4>
+<?php echo "Welcome to Preksha's Simple Bank, " . $_SESSION["user"]["email"];?>
