56ab5043dc8b92eb8835a16a3cf4a53834c3199d
pd0225 <pd389@njit.edu>
2020-07-30 17:52:24 -0400

    making changes

--- public_html/Milestone3_Project/footer.php
+++ public_html/Milestone3_Project/footer.php
@@ -1,4 +1,4 @@
-<footer style="padding-top: 100px; margin-left: 39pc;">
+<footer style="padding-top: 200px; margin-left: 39pc;">
 <div class="footer">
     <li><a href="contact.php">Contact</a></li> <li><a href="bankingtips.php">Online Banking Tips</a></li> <li>Copyright@PSB 2020</li>
 
