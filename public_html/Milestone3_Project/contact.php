9dcc6edc6a90e768e719a0a8ecb048899ee09969
pd0225 <pd389@njit.edu>
2020-07-31 18:06:46 -0400

    updating

--- public_html/Milestone3_Project/contact.php
+++ public_html/Milestone3_Project/contact.php
@@ -15,22 +15,22 @@
 </head>
 
 <div>
-    <h2 style="text-align:center;color:black;">Contact Us</h2>
+    <h2 style="text-align:center;color:black; padding-bottom: 20px">Contact Us</h2>
 
         <h3 style="color:darkslategrey; text-align: center;"><u>India Branch</u></h3>
         <p style="text-align: center"><span class="heading">Address: </span>PSB, Maharashtra, Mumbai</p>
         <p style="text-align: center"><span class="heading">Phone: </span>+91 9123 4567</p>
-        <p style="text-align: center"><span class="heading">Email: </span>psbbranch.in@psb.co</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.in@psb.co</p>
 
         <h3 style="color:darkslategrey; text-align: center;"><u>Australia Branch</u></h3>
         <p style="text-align: center"><span class="heading"> Address: </span>PSB, Sydney, Australia</p>
         <p style="text-align: center"><span class="heading"> Phone: +61 0245 6789</span></p>
-        <p style="text-align: center"><span class="heading"> Email: psbbranch.au@psb.co</span></p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading"> Email: psbbranch.au@psb.co</span></p>
 
         <h3 style="color:darkslategrey; text-align: center;"><u>Canada Branch</u></h3>
         <p style="text-align: center"><span class="heading">Address: </span>PSB, Ontario, Canada</p>
         <p style="text-align: center"><span class="heading">Phone: </span>+1 226 1234</p>
-        <p style="text-align: center"><span class="heading">Email: </span>psbbranch.ca@psb.co</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.ca@psb.co</p>
 
 </div>
 
