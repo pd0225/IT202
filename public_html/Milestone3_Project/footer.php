8d971f2c90dcba59f0a578d527c6a86887da6b95
pd0225 <pd389@njit.edu>
2020-07-31 16:45:28 -0400

    fixed error

--- public_html/Milestone3_Project/footer.php
+++ public_html/Milestone3_Project/footer.php
@@ -1,6 +1,6 @@
 <footer style="padding-top: 200px; text-align: center; margin: 5px; padding-bottom: 15px;">
     <div class="footer"></div>
-    Copyright@PSB 2020_____ <a href="contact.php">Contact_____</a> <a href="bankingtips.php">Online Banking Tips_____</a> <a href=""administrator.php">Admin_____</a>
+    Copyright@PSB 2020_____ <a href="contact.php">Contact_____</a> <a href="bankingtips.php">Online Banking Tips_____</a> <a href="administrator.php">Admin_____</a>
 
 </footer>
 
