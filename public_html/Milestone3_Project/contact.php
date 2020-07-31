3115e5cdc74d16d2af55946813455b2381a52d96
pd0225 <pd389@njit.edu>
2020-07-31 18:10:37 -0400

    making changes

--- public_html/Milestone3_Project/contact.php
+++ public_html/Milestone3_Project/contact.php
@@ -23,9 +23,9 @@
         <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.in@psb.co</p>
 
         <h3 style="color:darkslategrey; text-align: center;"><u>Australia Branch</u></h3>
-        <p style="text-align: center"><span class="heading"> Address: </span>PSB, Sydney, Australia</p>
-        <p style="text-align: center"><span class="heading"> Phone: +61 0245 6789</span></p>
-        <p style="text-align: center; padding-bottom: 10px;"><span class="heading"> Email: psbbranch.au@psb.co</span></p>
+        <p style="text-align: center"><span class="heading">Address: </span>PSB, Sydney, Australia</p>
+        <p style="text-align: center"><span class="heading">Phone: </span>+61 0245 6789</p>
+        <p style="text-align: center; padding-bottom: 10px;"><span class="heading">Email: </span>psbbranch.au@psb.co</p>
 
         <h3 style="color:darkslategrey; text-align: center;"><u>Canada Branch</u></h3>
         <p style="text-align: center"><span class="heading">Address: </span>PSB, Ontario, Canada</p>
