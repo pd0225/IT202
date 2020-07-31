1de106bf2747b446c0e80aabbf27149ef29815ba
pd0225 <pd389@njit.edu>
2020-07-31 19:10:49 -0400

    making changes

--- public_html/Milestone3_Project/acc_prof.php
+++ public_html/Milestone3_Project/acc_prof.php
@@ -1,7 +1,7 @@
 <?php
 require("config.php");
-include("header.php");
 require("common.inc.php");
+include("header.php");
 
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 $db = new PDO($connection_string, $dbuser, $dbpass);
