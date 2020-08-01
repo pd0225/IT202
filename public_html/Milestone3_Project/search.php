84ec8020b422d4db22ee8c48c3cd5c2598e6ceee
pd0225 <pd389@njit.edu>
2020-08-01 17:12:18 -0400

    making changes

--- public_html/Milestone3_Project/search.php
+++ public_html/Milestone3_Project/search.php
@@ -41,7 +41,7 @@ require ("common.inc.php");
         <?php foreach($results as $row):?>
             <li>
                 <?php echo get($row, "name")?>
-                <?php echo get($row, "AccountBalance");?>
+                <?php echo get($row, "Balance");?>
                 <a href="delete.php?accountId=<?php echo get($row, "id");?>">Delete</a>
             </li>
         <?php endforeach;?>
