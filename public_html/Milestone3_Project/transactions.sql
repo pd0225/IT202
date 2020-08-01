c8fe8bb9adf38f22b041c2a1c5b63b325c7f4b06
pd0225 <pd389@njit.edu>
2020-08-01 15:43:12 -0400

    updating project'

---
+++ public_html/Milestone3_Project/transactions.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Transactions (
+    id int auto_increment,
+    acc_src_id int not null,
+    acc_dest_id int null,
+    amount decimal(12,2),
+    acctype varchar(10),
+    memo TEXT,
+    exp_total decimal (12,2),
+    created datetime default current_timestamp,
+    primary key (id),
+    foreign key (acc_src_id) references Accounts(id),
+    foreign key (acc_dest_id) references Accounts(id)
+);
