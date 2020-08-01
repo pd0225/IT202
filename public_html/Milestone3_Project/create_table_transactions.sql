a32e51773fbf9d8a282e68899ed771b08909493a
pd0225 <pd389@njit.edu>
2020-08-01 15:49:02 -0400

    making changes

--- public_html/Milestone3_Project/create_table_transactions.sql
+++ public_html/Milestone3_Project/create_table_transactions.sql
@@ -9,6 +9,6 @@ CREATE TABLE Transactions (
     exp_total decimal (12,2),
     created datetime default current_timestamp,
     primary key (id),
-    foreign key (act_src_id) references Accounts(id),
-    foreign key (act_dest_id) references Accounts(id)
+    foreign key (acc_src_id) references Accounts(id),
+    foreign key (acc_dest_id) references Accounts(id)
 )
